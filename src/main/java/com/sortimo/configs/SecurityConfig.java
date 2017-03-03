5ac358d05bd763f8e9f2a801b4ca384fffab5c68
Jody <jlerch@sortimo.de>
2017-03-03 11:18:42 +0100

    security: Profile gesteuert. Bei dev wird nicht authentifiziert.

--- src/main/java/com/sortimo/configs/SecurityConfig.java
+++ src/main/java/com/sortimo/configs/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sortimo.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 import com.sortimo.services.MyUserDetailsService;
 
+@Profile("!dev")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
