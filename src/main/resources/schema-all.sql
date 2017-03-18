41a3607d449b5b743757b2fa7f24c2a05a831214
Jody <jlerch@sortimo.de>
2017-03-18 17:28:00 +0100

    schema-all.sql angepasst Passwort Länge auf 255 erhöht.

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
   id int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   username varchar(45) NOT NULL,
-  password varchar(45) NOT NULL,
+  password varchar(255) NOT NULL,
   lastname varchar(45) NOT NULL,
   firstname varchar(45) NOT NULL,
   email varchar(45) NOT NULL
