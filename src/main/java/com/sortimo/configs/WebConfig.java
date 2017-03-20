5a4e1f1b38c741d9ba9e9a600caadc266d0d169f
Jody <johannes.lerch@buttinette.de>
2017-03-20 19:10:17 +0100

    CORS Requests global für localhost:3000 erlaubt.

--- src/main/java/com/sortimo/configs/WebConfig.java
+++ src/main/java/com/sortimo/configs/WebConfig.java
@@ -1,8 +1,23 @@
 package com.sortimo.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/*").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+	
 	
 }
