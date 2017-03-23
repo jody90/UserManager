617f90f6bc8298f6dfdc0495d313ac7168f5c175
Jody <johannes.lerch@buttinette.de>
2017-03-23 22:16:12 +0100

    CORS alle URLs erlauben.

--- src/main/java/com/sortimo/configs/WebConfig.java
+++ src/main/java/com/sortimo/configs/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/*").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/api/*").allowedOrigins("*");
             }
         };
     }
